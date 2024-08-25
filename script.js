e651a9522a80bcad98787d7f4823382866d68d95
philjot <philemon_opoku@yahoo.com>
2024-08-25 19:13:03 +0100

    sort added

--- script.js
+++ script.js
@@ -39,4 +39,8 @@ function readSentence() {
   const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
   const sortedArray = insertionSort(unsortedArray);
   console.log(sortedArray); // Output: [11, 12, 22, 25, 34, 64, 90]
+
+
+
+  
   
